e991efa887a763421070ebbf77a05ac7e69a7579
GeebeeFrost <omgbolahan1@gmail.com>
2023-12-26 12:18:46 +0100

    Fix 8-cities_by_states task

--- models/state.py
+++ models/state.py
@@ -19,7 +19,7 @@ class State(BaseModel, Base):
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship(
-            "City", cascade="all, delete-orphan", backref="state"
+            "City", cascade="all, delete-orphan", backref="states"
         )
     else:
         name = ""
