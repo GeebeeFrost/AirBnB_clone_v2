1785d2c0855779121ce5af6590cbb93d6cf3dd15
GeebeeFrost <omgbolahan1@gmail.com>
2023-12-28 07:18:52 +0000

    Add definition for route state/<id>

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,14 +17,19 @@ def close_session(self):
 def display_states():
     """Displays a HTML page with a list of states present in database"""
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('9-states.html', mode='all', states=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def display_state_cities(id):
     """Displays a HTML page with a state and cities linked to that state"""
-    
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template(
+                    '9-states.html', mode='single', single_state=state
+                    )
+    return render_template('9-states.html', mode='none')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
