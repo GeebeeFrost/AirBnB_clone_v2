81bee3ef37ab0ee5f55cffbd235cf99805a198d5
GeebeeFrost <omgbolahan1@gmail.com>
2023-12-28 07:43:22 +0100

    Start task 10

--- models/state.py
+++ models/state.py
@@ -19,7 +19,7 @@ class State(BaseModel, Base):
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship(
-            "City", cascade="all, delete-orphan", backref="states"
+            "City", cascade="all, delete-orphan", backref="state"
         )
     else:
         name = ""
---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """Removes the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def display_states():
+    """Displays a HTML page with a list of states present in database"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def display_state_cities(id):
+    """Displays a HTML page with a state and cities linked to that state"""
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
